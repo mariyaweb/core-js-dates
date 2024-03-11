1040ee833592903392df4b5f9a9ce7c6904fd160
github-mariyaweb <96mariyask@gmail.com>
2024-03-11 21:02:32 +0300

    feat: add 2 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -32,8 +32,12 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const convertedDate = new Date(date);
+  const h = convertedDate.getHours().toString().padStart(2, '0');
+  const min = convertedDate.getMinutes().toString().padStart(2, '0');
+  const sec = convertedDate.getSeconds().toString().padStart(2, '0');
+  return `${h}:${min}:${sec}`;
 }
 
 /**

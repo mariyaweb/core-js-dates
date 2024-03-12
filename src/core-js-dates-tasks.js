6caccd53fe30a01384c08a1be08048f890fe5a06
github-mariyaweb <96mariyask@gmail.com>
2024-03-12 18:49:14 +0300

    feat: add 12 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -332,8 +332,11 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if (!(year % 4) && year % 100) return true;
+  if (year % 400) return false;
+  return true;
 }
 
 module.exports = {

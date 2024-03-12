50cb8832d315bd02c7aa7339886f05dbd344e192
github-mariyaweb <96mariyask@gmail.com>
2024-03-12 10:58:37 +0300

    feat: add 5 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -97,8 +97,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const date = new Date(year, month);
+  return new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 }
 
 /**

b4296fd861e69cd63b8837d8e4647f5288177061
github-mariyaweb <96mariyask@gmail.com>
2024-03-12 11:12:01 +0300

    feat: add 6 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -113,8 +113,12 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart).getTime();
+  const end = new Date(dateEnd).getTime();
+  const diff = end - start;
+  const diffDays = diff / (1000 * 3600 * 24);
+  return diffDays + 1;
 }
 
 /**
